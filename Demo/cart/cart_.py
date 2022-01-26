f6ce249b2da1e4097ac5731a01f4a37722837dd3
jeyu54217 <73396926+jeyu54217@users.noreply.github.com>
2022-01-26 16:16:41 +0800

    Update cart_.py

--- Demo/cart/cart_.py
+++ Demo/cart/cart_.py
@@ -2,12 +2,13 @@ from decimal import Decimal
 from django.conf import settings
 from shop.models import Product
 
+## All request is design for func. in cart.views
+
 class Cart():
     def __init__(self,request):
         self.session = request.session
         cart = self.session.get(settings.CART_SESSION_ID)
         if not cart:
-            # save an empty cart in the session
             cart = self.session[settings.CART_SESSION_ID] = {}
         self.cart = cart 
 
@@ -15,6 +16,7 @@ class Cart():
         product_id = str(product.id)
         product_price = str(product.price)
         quantity = int(quantity)
+        
         if product_id not in self.cart:
             self.cart[product_id] = {
                 'quantity': 0,
@@ -27,9 +29,6 @@ class Cart():
         self.save()
 
     def save(self):
-        """
-        Update cart
-        """
         self.session[settings.CART_SESSION_ID] = self.cart
         self.session.modified = True
 
@@ -38,7 +37,10 @@ class Cart():
         if product_id in self.cart:
             del self.cart[product_id]
             self.save()
-
+            
+    def get_total_price(self):
+        return sum(Decimal(item['price']) * item['quantity'] for item in self.cart.values())
+    
     def __iter__(self):
         product_ids = self.cart.keys()
         products = Product.objects.filter(id__in=product_ids)
@@ -54,13 +56,7 @@ class Cart():
     def __len__(self):
         return sum(item['quantity'] for item in self.cart.values())
 
-    def get_total_price(self):
-        return sum(Decimal(item['price']) * item['quantity'] for item in self.cart.values())
-
     def clear(self):
-        """
-        Empty cart
-        """
         del self.session[settings.CART_SESSION_ID]
         self.session.modified = True
 
